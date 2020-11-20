4b888d7a3273b52e62757cba7018fbd3390df77f
Yury Hryshanau <mr.deusvult@gmail.com>
2020-11-20 05:26:07 +0300

    Upload New File

---
+++ ClassesAndObjectsTask/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassesAndObjectsTask
+{
+    public class Employee
+    {
+        private string surname;
+        private int age;
+
+        public Employee()
+        {
+        }
+
+        public Employee(string surname, int age)
+        {
+            this.surname = surname;
+            this.age = age;
+        }
+
+        private string GetEmployeeAge() => age.ToString();
+
+        public void ChangeEmployeeSurname(string newSurname)
+        {
+            surname = newSurname;
+        }
+
+        public string InfoEmployee() => $"Surname: {surname}, Age: {age}";
+    }
+}
